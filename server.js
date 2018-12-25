23b73ee9af55e665146140f9b065615de67986e5
Javier Brea <javier.brea@gmail.com>
2018-12-25 12:25:23 +0100

    Add dht-sensor

---
+++ server.js
@@ -0,0 +1,81 @@
+'use strict'
+
+const path = require('path')
+
+const domapic = require('domapic-service')
+
+const pluginConfigs = require('./lib/plugins.json')
+
+const options = require('./lib/options')
+const Sensor = require('./lib/Sensor')
+
+const {
+  TEMPERATURE_NAME,
+  TEMPERATURE_EVENT,
+  TEMPERATURE_STATE,
+  HUMIDITY_NAME,
+  HUMIDITY_EVENT,
+  HUMIDITY_STATE,
+  INTERVAL_OPTION,
+  EVENTS_STEP_OPTION,
+  SENSOR_TYPE_OPTION,
+  GPIO_OPTION
+} = require('./lib/statics')
+
+domapic.createModule({
+  packagePath: path.resolve(__dirname),
+  customConfig: options
+}).then(async dmpcModule => {
+  const config = await dmpcModule.config.get()
+
+  const sensor = new Sensor(
+    config[SENSOR_TYPE_OPTION],
+    config[GPIO_OPTION],
+    config[INTERVAL_OPTION],
+    config[EVENTS_STEP_OPTION],
+    dmpcModule.tracer
+  )
+
+  await dmpcModule.register({
+    [TEMPERATURE_NAME]: {
+      description: TEMPERATURE_STATE,
+      data: {
+        type: 'number'
+      },
+      state: {
+        description: TEMPERATURE_STATE,
+        handler: () => sensor.temperature
+      },
+      event: {
+        description: TEMPERATURE_EVENT
+      }
+    },
+    [HUMIDITY_NAME]: {
+      description: HUMIDITY_STATE,
+      data: {
+        type: 'number'
+      },
+      state: {
+        description: HUMIDITY_STATE,
+        handler: () => sensor.humidity
+      },
+      event: {
+        description: HUMIDITY_EVENT
+      }
+    }
+  })
+
+  sensor.events.on(TEMPERATURE_NAME, newValue => {
+    dmpcModule.tracer.debug(TEMPERATURE_EVENT, newValue)
+    dmpcModule.events.emit(TEMPERATURE_NAME, newValue)
+  })
+
+  sensor.events.on(HUMIDITY_NAME, newValue => {
+    dmpcModule.tracer.debug(HUMIDITY_EVENT, newValue)
+    dmpcModule.events.emit(HUMIDITY_NAME, newValue)
+  })
+
+  await dmpcModule.addPluginConfig(pluginConfigs)
+
+  return dmpcModule.start()
+})
--- test/unit/server.specs.js
+++ test/unit/server.specs.js
@@ -1,8 +1,33 @@
+const path = require('path')
+
 const test = require('narval')
 
+const DomapicMocks = require('./Domapic.mocks')
+const SensorMocks = require('./lib/Sensor.mocks')
+
 test.describe('server', () => {
-  test.it('should exists', () => {
+  let domapic
+  let sensor
+  // let abilities
+
+  test.before(async () => {
+    domapic = new DomapicMocks()
+    sensor = new SensorMocks()
+    domapic.stubs.module.config.get.resolves({})
     require('../../server')
-    test.expect(true).to.be.true()
+    domapic.utils.executeModuleListener(domapic.stubs.module)
+    await domapic.utils.resolveOnStartCalled()
+    // abilities = domapic.stubs.module.register.getCall(0).args[0]
+  })
+
+  test.after(() => {
+    sensor.restore()
+    domapic.restore()
+  })
+
+  test.describe('when required', () => {
+    test.it('should have created a Domapic Module, passing the package path', () => {
+      test.expect(domapic.stubs.createModule.getCall(0).args[0].packagePath).to.equal(path.resolve(__dirname, '..', '..'))
+    })
   })
 })
